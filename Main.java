72f370ec1557f3a54b9bdd8c8dcb888050719e2f
Paul <Paul-Ludwig.Simon@t-online.de>
2025-01-31 22:31:54 +0100

    Update Main.java
    am back baby

--- Main.java
+++ Main.java
@@ -1,9 +1,27 @@
+import static java.lang.System.out;
+
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    //Code goes here
-    
-  }
-  
+        /*
+        Casino casaBlanca = new Casino();
+        Account acc1 = new Account("PaulTheJavaDev", "HelloWorld!", 10000);
+        Account acc2 = new Account("Yayayayay", "HelloWorld!", 10000);
+        Account acc3 = new Account("PaulTheJavaDev", "HelloWorld!", 10000);
+
+
+        out.println(Account.users);
+         */
+
+        /*
+        Account acc1 = new Account("PaulTheJavaDev", "HelloWorld!", 10000);
+        int password = acc1.returnBalance();
+        out.println(password);
+
+        Casino.createAccount();
+         */
+        Casino.createAccount();
+    }
+
 }
